b67da87dc4dd29d237850856051d240ed3d58632
Jonesh Shrestha <joneshshrestha@Joneshs-MacBook-Pro.local>
2020-08-07 15:52:08 +0545

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+import { quotesArray } from './quotes.js'
+
+
+
+document.getElementById('btn').addEventListener('click', () => {
+    const randomNumberGenerator = Math.floor(Math.random() * quotesArray.length)
+    document.querySelector('p').textContent = quotesArray[randomNumberGenerator]
+})
+
