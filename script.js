6dd707dcf5acebf315128d919899bcdeb286d47f
joneshshrestha <contact@joneshshrestha.com>
2020-08-12 09:04:09 +0545

    update

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 import { quotesArray } from './quotes.js'
 
-
-
 document.getElementById('btn').addEventListener('click', () => {
     const randomNumberGenerator = Math.floor(Math.random() * quotesArray.length)
     document.querySelector('p').textContent = quotesArray[randomNumberGenerator]
